bbec68f974e835e3dbe77dadd28c608848d59c2e
digitallysavvy <hermes.frangoudis@gmail.com>
2024-06-27 17:19:26 -0400

    use StartRecordingResponse struct

--- cloud_recording_service/cloud_recording_handler_start_recording.go
+++ cloud_recording_service/cloud_recording_handler_start_recording.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 )
 
-// HandleStartRecordingReq handles the start recording request.
-// It uses the makeRequest function to send the request to the Agora cloud recording API.
+// HandleStartRecordingReq processes the request to start a cloud recording session in Agora's cloud recording service.
+// It validates the request parameters, constructs the request URL, and use makeRequest to send data to Agora Endpoint
 //
 // Parameters:
 //   - startReq: StartRecordingRequest - The request payload for starting a recording.
@@ -17,19 +17,18 @@ import (
 //   - string: The recording ID (sid) acquired from the Agora cloud recording API.
 //   - error: An error object if any issues occurred during the request process.
 func (s *CloudRecordingService) HandleStartRecordingReq(startReq StartRecordingRequest, resourceId string, modeType string) (string, error) {
+
+	// build start recording endpoint
 	url := fmt.Sprintf("%s/%s/cloud_recording/resourceid/%s/mode/%s/start", s.baseURL, s.appID, resourceId, modeType)
+
+	// send request to start endpoint
 	body, err := s.makeRequest("POST", url, startReq)
 	if err != nil {
 		return "", err
 	}
 
 	// Parse the response body to extract the necessary information
-	var response struct {
-		Cname      string `json:"cname"`
-		Uid        string `json:"uid"`
-		ResourceId string `json:"resourceId"`
-		Sid        string `json:"sid"`
-	}
+	var response StartRecordingResponse
 	err = json.Unmarshal(body, &response)
 	if err != nil {
 		return "", fmt.Errorf("error parsing response: %v", err)
